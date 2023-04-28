1e40f24bff1d53b028aee42cc421a9c2b3c53016
Ekene <ekene2003@users.noreply.github.com>
2023-04-28 15:55:08 +0100

    done with index.php

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function singleelem(elem) {
+     return document.querySelector(elem);
+};
+function multielem(elem) {
+     return document.querySelectorAll(elem);
+};
+let homeWrap = singleelem(".home");
+let indexheader = singleelem(".index-header");
+let bg = ["#ff6700", "#ff3016", "#6610f2"];
+i = 0
+setInterval(() => {
+     i += 1;
+     indexheader.style.background = bg[i];
+     let newWrap = homeWrap.style.right = ` ${i}00%`;
+      if (homeWrap.style.right == "200%") {
+           i = -1;
+      }
+}, 6000);
