5b77f74ad00aedfe5b773b3d23274da2777bc001
Ekene <ekene2003@users.noreply.github.com>
2023-05-11 17:18:19 +0100

    sorting cart btn out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,85 @@
 function singleelem(elem) {
-     return document.querySelector(elem);
-};
+  return document.querySelector(elem);
+}
 function multielem(elem) {
-     return document.querySelectorAll(elem);
-};
+  return document.querySelectorAll(elem);
+}
 let homeWrap = singleelem(".home");
 let indexheader = singleelem(".index-header");
 let formToggler = singleelem(".login-small");
 let cartamount = singleelem(".badge-no");
 let drinksBtn = multielem(".drink-btn");
 let drinks = multielem(".drinks");
+let increaseBtn = multielem(".increase");
+let decrease = multielem(".decrease");
+let amount = multielem(".amount");
 let cartBtn = multielem(".cart");
-b=0;
-cartBtn.forEach(btn => {
-     btn.onclick=()=>{
-          b++;
-       cartamount.innerHTML=`
+let minicart = multielem(".mini-cart");
+amount.forEach((am) => {
+     am.innerText = `0`;
+   });
+   c=0;
+
+minicart.forEach((cart) => {
+  increaseBtn.forEach((btn) => {
+     let current = btn.closest(".minicart-btn").querySelector(".amount");
+    btn.onclick = () => {
+            c+=1;
+      current.innerText = `${c}`;
+      console.log(current.innerText);
+      current.innerText = 0;
+    };
+  });
+
+});
+
+b = 0;
+cartBtn.forEach((btn) => {
+  btn.onclick = () => {
+    b++;
+    cartamount.innerHTML = `
        <a class="user-link"><i class="fa fa-cart-shopping"></i></a>
        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
          ${b}
          <span class="visually-hidden">unread messages</span>
        </span>
        `;
-       if (b<=0) {
-          cartamount.innerHTML=`
+    if (b <= 0) {
+      cartamount.innerHTML = `
           <a class="user-link"><i class="fa fa-cart-shopping"></i></a>
           <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            ${b==0}
+            ${b == 0}
             <span class="visually-hidden">unread messages</span>
           </span>
           `;
-       }
-
-     }
+    }
+  };
 });
-drinksBtn.forEach(btn => {
-     btn.onclick=()=>{
-     let category = btn.dataset.category;
-        console.log(category);
-        drinks.forEach(drink => {
-          let drinkType = drink.dataset.type;
-          if (category =="all") {
-               drink.style.display = "block";
-          }
-          else{
-               if(category == drinkType){
-                    drink.style.display = "block";
-               }
-               else{
-                    drink.style.display = "none"
-               }
-          }
-     });
-     }
-   
+drinksBtn.forEach((btn) => {
+  btn.onclick = () => {
+    let category = btn.dataset.category;
+    console.log(category);
+    drinks.forEach((drink) => {
+      let drinkType = drink.dataset.type;
+      if (category == "all") {
+        drink.style.display = "block";
+      } else {
+        if (category == drinkType) {
+          drink.style.display = "block";
+        } else {
+          drink.style.display = "none";
+        }
+      }
+    });
+  };
 });
 let bg = ["#ff6700", "#ff3016", "#6610f2"];
-i = 0
+i = 0;
 setInterval(() => {
-     i += 1;
-     indexheader.style.background = bg[i];
-     let newWrap = homeWrap.style.right = ` ${i}00%`;
-      if (homeWrap.style.right == "200%") {
-           i = -1;
-      }
+  i += 1;
+  indexheader.style.background = bg[i];
+  let newWrap = (homeWrap.style.right = ` ${i}00%`);
+  if (homeWrap.style.right == "200%") {
+    i = -1;
+  }
 }, 6000);
