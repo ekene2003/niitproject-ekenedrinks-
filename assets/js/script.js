75e6154049afe587188b07f52a3564334bc721d2
Ekene <ekene2003@users.noreply.github.com>
2023-05-13 13:18:37 +0100

    new feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,38 +10,10 @@ let formToggler = singleelem(".login-small");
 let cartamount = singleelem(".badge-no");
 let drinksBtn = multielem(".drink-btn");
 let drinks = multielem(".drinks");
-let increaseBtn = multielem(".increase");
-let decreaseBtn = multielem(".decrease");
-let amountPage = multielem(".amount");
 let cartBtn = multielem(".cart");
-let minicart = multielem(".mini-cart");
 
+let cartWrap = singleelem(".mini-cart-wrap");
 c=0;
-increaseBtn.forEach((btn) => {
-  btn.onclick = () => {
-   c += 1;
-   let current = btn.closest(".mini-sec").querySelector(".amount");
-   current.innerText = `${c}`;
-   console.log(current.innerText);
-  };
-});
-decreaseBtn.forEach((btn) => {
-  btn.onclick = () => {
-   c -= 1;
-   let current = btn.closest(".mini-sec").querySelector(".amount");
-   let currentCart = btn.closest(".mini-cart");
-   if (current.innerText <= 1) {
-    current.innerText = 0;
-    c = 0;
-    setTimeout(() => {
-   currentCart.style.display="none";
-    }, 1000);
-   };
-   current.innerText = `${c}`;
-   console.log(current.innerText);
-
-  };
-});
 b = 0;
 cartBtn.forEach((btn) => {
   btn.onclick = () => {
@@ -53,15 +25,65 @@ cartBtn.forEach((btn) => {
          <span class="visually-hidden">unread messages</span>
        </span>
        `;
-    if (b <= 0) {
-      cartamount.innerHTML = `
-          <a class="user-link"><i class="fa fa-cart-shopping"></i></a>
-          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            ${b = 0}
-            <span class="visually-hidden">unread messages</span>
-          </span>
-          `;
-    }
+    cartWrap.innerHTML +=`
+                                <div class="mini-cart">
+                                   <div class="cart-wrap">
+                                        <div class="col-md-4">
+                                             <img src="./assets/images/Coca Cola.jpeg" alt="" class="cart-img">
+                                        </div>
+                                        <div class="col-md-4 mini-sec">
+                                             <h3 class="minicart-head">Oceaneyes 4</h3>
+                                             <p class="minicart-p">Beer</p>
+                                             <p class="mincart-p">$40</p>
+                                             <div class="minicart-btn">
+                                                  <div class="increase"><i class="fa fa-plus"></i></div>
+                                                  <div class="vr"></div>
+                                                  <div class="amount"> 1</div>
+                                                  <div class="vr"></div>
+                                                  <div class="decrease"><i class="fa fa-minus"></i></div>
+                                             </div>
+                                        </div>
+                                        <div class="col-md-4 remove-sec">
+                                             <div class="removeBtn"><p>Remove</p></div>
+                                        </div>
+                                   </div>
+                              </div>            
+    `;
+    let increaseBtn = multielem(".increase");
+let decreaseBtn = multielem(".decrease");
+let amountPage = multielem(".amount");
+let minicart = multielem(".mini-cart");
+    increaseBtn.forEach((btn) => {
+      btn.onclick = () => {
+       c += 1;
+       let current = btn.closest(".mini-sec").querySelector(".amount");
+       current.innerText = `${c}`;
+       console.log(current.innerText);
+      };
+    });
+    decreaseBtn.forEach((btn) => {
+      btn.onclick = () => {
+       c -= 1;
+       let current = btn.closest(".mini-sec").querySelector(".amount");
+       let currentCart = btn.closest(".mini-cart");
+       if (current.innerText <= 1) {
+        current.innerText = 0;
+        c = 0;
+        setTimeout(() => {
+       currentCart.style.display="none";
+        }, 1000);
+        cartamount.innerHTML = `
+        <a class="user-link"><i class="fa fa-cart-shopping"></i></a>
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          ${b-=1}
+          <span class="visually-hidden">unread messages</span>
+        </span>
+        `;
+       };
+       current.innerText = `${c}`;
+       console.log(current.innerText);
+      };
+    });
   };
 });
 drinksBtn.forEach((btn) => {
