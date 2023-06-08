13b1409e2c88250ddaa3108388142b5c3d1d89fa
Ekene <ekene2003@users.noreply.github.com>
2023-06-08 14:02:40 +0100

    new project

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,15 @@
 require_once("./includes/header.php")
 ?>
 <body>
-    
+    <header class="dashboard-header">
+        <div class="dashboard-head-p">
+        <p class="dashboard-p">Pages/Profile</p>
+            <h3 class="dashboard-p-head">Profile</h3>
+        </div>
+        <form action="" class="">
+            <input type="search" name="" id="" placeholder="type here">
+        </form>
+    </header>
     
 </body>
 </html>
