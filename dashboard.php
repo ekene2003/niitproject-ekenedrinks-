f3b3d07ccb523f2c2c5925f6db16e5db272a33c1
Ekene <ekene2003@users.noreply.github.com>
2023-06-08 15:23:26 +0100

    new feature

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,19 @@ require_once("./includes/header.php")
         <form action="" class="">
             <input type="search" name="" id="" placeholder="type here">
         </form>
+        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    ...
+  </div>
+</div>
     </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     
 </body>
 </html>
