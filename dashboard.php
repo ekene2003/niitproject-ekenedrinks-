354599525a24b181e0af42490255fe8d664b9603
Ekene <ekene2003@users.noreply.github.com>
2023-06-13 04:26:33 +0100

    making my header

--- dashboard.php
+++ dashboard.php
@@ -1,30 +1,31 @@
 <?php
-require_once( './includes/header.php' );
+require_once('./includes/header.php');
 
 ?>
-<body>
-<link rel = 'stylesheet' href = './assets/'>
-<header class = '.dashboard-header'>
-<div class = '.dashboard-head-p'>
-<p class = '.dashboard-p'>Pages/Profile</p>
-<h3 class = '.dashboard-p-head'>Profile</h3>
-</div>
-<form action = '' class = ''>
-<input type = 'search' name = '' id = '' placeholder = 'type here'>
-</form>
-<button class = 'btn btn-primary' type = 'button' data-bs-toggle = 'offcanvas' data-bs-target = '#offcanvasRight' aria-controls = 'offcanvasRight'>Toggle right offcanvas</button>
 
-<div class = 'offcanvas offcanvas-start' tabindex = '-1' id = 'offcanvasRight' aria-labelledby = 'offcanvasRightLabel'>
-<div class = 'offcanvas-header'>
-<h5 class = 'offcanvas-title' id = 'offcanvasRightLabel'><i class = 'fa fa-'></i></h5>
-<button type = 'button' class = 'btn-close' data-bs-dismiss = 'offcanvas' aria-label = 'Close'></button>
-</div>
-<div class = 'offcanvas-body'>
-...
-</div>
-</div>
-</header>
-<script src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js'></script>
+<body>
+  <link rel='stylesheet' href='./assets/'>
+  <header class='.dashboard-header d-flex justify-content-between bg-secondary p-3'>
+    <div class='.dashboard-head-p'>
+      <p class='.dashboard-p'>Pages/Profile</p>
+      <h3 class='.dashboard-p-head'>Profile</h3>
+    </div>
+    <form action='' class='ms-5 flex-5 dashboard-search'>
+      <input type='search' name='' id='' placeholder='type here'>
+    </form>
+    <button class='btn btn-primary' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasRight' aria-controls='offcanvasRight'>Toggle right offcanvas</button>
+    <div class='offcanvas offcanvas-start' tabindex='-1' id='offcanvasRight' aria-labelledby='offcanvasRightLabel'>
+      <div class='offcanvas-header'>
+        <h5 class='offcanvas-title' id='offcanvasRightLabel'><i class='fa fa-'></i></h5>
+        <button type='button' class='btn-close' data-bs-dismiss='offcanvas' aria-label='Close'></button>
+      </div>
+      <div class='offcanvas-body'>
+        ...
+      </div>
+    </div>
+  </header>
+  <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js'></script>
 
 </body>
+
 </html>
