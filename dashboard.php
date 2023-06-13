a382d09330695397365f6198914db923ce8ad49f
Ekene <ekene2003@users.noreply.github.com>
2023-06-13 04:13:32 +0100

    working on my dashboard header

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,13 @@
 <?php 
-require_once("./includes/header.php")
+require_once("./includes/header.php");
+
 ?>
 <body>
-    <header class="dashboard-header">
-        <div class="dashboard-head-p">
-        <p class="dashboard-p">Pages/Profile</p>
-            <h3 class="dashboard-p-head">Profile</h3>
+  <link rel="stylesheet" href="./assets/">
+    <header class=".dashboard-header">
+        <div class=".dashboard-head-p">
+        <p class=".dashboard-p">Pages/Profile</p>
+            <h3 class=".dashboard-p-head">Profile</h3>
         </div>
         <form action="" class="">
             <input type="search" name="" id="" placeholder="type here">
